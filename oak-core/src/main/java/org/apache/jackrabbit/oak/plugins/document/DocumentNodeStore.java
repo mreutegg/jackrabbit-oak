1e822f5ccac03daba0ea47f717bfd65bbd17c5a9
Marcel Reutegger <mreutegg@adobe.com>
2016-04-18 17:26:16 +0200

    OAK-3712: Clean up old and uncommitted changes
    Run background sweep only when not read-only

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -1986,7 +1986,7 @@ public final class DocumentNodeStore
     //----------------------< background sweep operation >----------------------
 
     void runBackgroundSweepOperations() {
-        if (isDisposed.get()) {
+        if (readOnlyMode || isDisposed.get()) {
             return;
         }
         try {
